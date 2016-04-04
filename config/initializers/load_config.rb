a2807b8f09823181a95021672db8e7c0d6d88e8f
MD <mattdwnet@gmail.com>
2016-04-04 12:42:23 +0200

    fix ENV config for heroku compatibility

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,6 @@
+APP_CONFIG['display_app_name'] = ENV['DISPLAY_APP_NAME']
+APP_CONFIG['app_domain'] = ENV['APP_DOMAIN']
+
 # load reputation
 APP_REPUTATION = YAML.load_file("#{Rails.root.to_s}/config/reputation.yml")
 
@@ -9,4 +12,3 @@ APP_BADGES_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/badges_config.yml"
 
 # load voting config
 APP_VOTING_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/voting_config.yml")
-
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Qaror::Application.config.secret_token = '632525dc58389d5ef567c2d5a0a901ec516d77efdb28db08547e200b789fa5791acb8de2c5cc1306a72b37a3165cc7b6be6e1fbb0b2c5652f9fae4bf0377a0d4'
+
+secret_token_dev = '632525dc58389d5ef567c2d5a0a901ec516d77efdb28db08547e200b789fa5791acb8de2c5cc1306a72b37a3165cc7b6be6e1fbb0b2c5652f9fae4bf0377a0d4'
+secret_token_prod = ENV["SECRET_KEY_BASE"]
+
+Qaror::Application.config.secret_token = Rails.env.production? ? secret_token_prod : secret_token_dev
